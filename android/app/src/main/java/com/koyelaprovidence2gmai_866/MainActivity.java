58722cec72c6c45d8b9feeb96ae035e0467e2f37
Crowdbotics <team@crowdbotics.com>
2019-01-31 22:30:49 +0000

    initial commit

---
+++ android/app/src/main/java/com/koyelaprovidence2gmai_866/MainActivity.java
@@ -0,0 +1,15 @@
+package com.koyelaprovidence2gmai_866;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "koyelaprovidence2gmai_866";
+    }
+}
